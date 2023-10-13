115bd96d17d3a4b098e37e54c8e882a0e6852e94
Souvik Saha <134754803+souviksaha13@users.noreply.github.com>
2023-10-13 19:02:12 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int val) {
+        data = val;
+        right = null;
+        left = null;
+    }
+}
+
+class GFG {
+
+    public static Node insert(Node tree, int val) {
+        Node temp = null;
+        if (tree == null) {
+            return new Node(val);
+        }
+
+        if (val < tree.data) {
+            tree.left = insert(tree.left, val);
+        } else if (val > tree.data) {
+            tree.right = insert(tree.right, val);
+        }
+
+        return tree;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            Node root = null;
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int k = sc.nextInt();
+                root = insert(root, k);
+            }
+
+            int s = sc.nextInt();
+
+            Solution obj = new Solution();
+            int ans = obj.floor(root, s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int floor(Node root, int x) {
+        // Code here
+        int[] ans = new int[1];
+        ans[0] = -1;
+        
+        inorder(root, x, ans);
+        
+        return ans[0];
+    }
+    
+    
+    void inorder(Node root, int x, int[] ans) {
+        if(root == null) return;
+        
+        inorder(root.left, x, ans);
+        if(root.data <= x) ans[0] = root.data;
+        inorder(root.right, x, ans);
+    }
+}
